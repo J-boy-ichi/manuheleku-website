6bcd41ac0f2874c0d51d1f24e3a864b086d58cc7
JO <110362625+Joichi508@users.noreply.github.com>
2025-08-20 00:05:03 +0900

    update

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/manuheleku-website/",
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
